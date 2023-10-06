5b17e5f35c9caa2e68a28a1af96fa1eaa30efc3f
Vincent Toon <vincenttoon22@gmail.com>
2023-10-06 17:57:46 -0500

    initial User added

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,58 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  booksRead: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "BooksRead",
+    },
+  ],
+  worms: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Worms",
+    },
+  ],
+});
+
+// set up pre-save middleware to create password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.virtual("wormCount").get(function() {
+    return this.worms.length;
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
