0ed958d74cd2cc6d614db04bf34b8f823f280330
Vincent Toon <vincenttoon22@gmail.com>
2023-10-06 17:02:38 -0500

    connection files added, switching branches

---
+++ server/config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/book-worm",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+module.exports = mongoose.connection;
---
+++ server/utils/auth.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
+
+module.exports = {
+  // middleware authentication function
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    // verifies JSON Web Token and matches it with data usage
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+
+    return req;
+  },
+  // assigns token to a user
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
